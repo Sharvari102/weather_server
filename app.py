5eec8aed463381bce0f60424d8ad889887ab14fa
Sharvari Basarkar <91872823+Sharvari102@users.noreply.github.com>
2023-12-06 10:53:48 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 from jinja2 import escape
-from itsdangerous import json
+from itsdangerous import json as _json
 
 
 
